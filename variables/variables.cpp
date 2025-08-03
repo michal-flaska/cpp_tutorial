54e265f3b44d881ff41a420dc5adb49ed8c0690b
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-08-03 11:09:39 +0200

    Update variables.cpp

--- variables/variables.cpp
+++ variables/variables.cpp
@@ -12,8 +12,11 @@ int main(){
 
         int x = 5;
         int y = 6;
+        int sum = x + y;
 
-        std::cout << x - y;
+        std::cout << x << '\n';
+        std::cout << y << '\n';
+        std::cout << sum << '\n';
 
         return 0;
 }
