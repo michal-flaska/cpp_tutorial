86eef4bf91f094ec269bc1d1441f65ba9eaf7ef8
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-08-03 16:56:45 +0200

    Create temperature-conversion.cpp

---
+++ tests/from-tutorial/temperature-conversion/temperature-conversion.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main() {
+
+        double temperature;
+        char unit; // Celsius or Fahrenheit
+
+        std::cout << "------------------ Temperature Calculator ------------------" << '\n';
+        std::cout << "F = Fahrenheit" << '\n';
+        std::cout << "C = Celsius" << '\n';
+        std::cout << "What unit would you like to convert to: ";
+        std::cin >> unit;
+
+        if(unit == 'F' || unit == 'f'){
+                std::cout << "Enter the temperature in Celsius: ";
+                std::cin >> temperature;
+                temperature = (1.8 * temperature) + 32.0; // Convert Celsius to Fahrenheit
+                std::cout << "Temperature is: " << temperature << "F" << '\n';
+
+        }
+        else if(unit == 'C' || unit == 'c'){
+                std::cout << "Enter the temperature in Fahrenheit: ";
+                std::cin >> temperature;
+                temperature = (temperature - 32.0) / 1.8; // Convert Fahrenheit to Celsius
+                std::cout << "Temperature is: " << temperature << "C" << '\n';
+        }
+        else {
+                std::cout << "Invalid unit. Please enter either 'F' or 'C'." << '\n';
+        }
+
+        std::cout << "------------------------------------------------------------" << '\n';
+        return 0;
+}
