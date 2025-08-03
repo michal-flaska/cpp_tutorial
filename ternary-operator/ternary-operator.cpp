9ba13a2a38e2d83b04d7cf571a255591d49cf359
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-08-03 16:24:36 +0200

    Create ternary-operator.cpp

---
+++ ternary-operator/ternary-operator.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+int main() {
+
+        /*
+        // EXAMPLE 1:
+
+        // Below is a code using if-else statements to check the grade and print the result.
+
+        int grade = 70;
+
+        if(grade >= 65){
+                std::cout << "You passed the exam!" << '\n';
+        } else {
+                std::cout << "You failed the exam." << '\n';
+        }
+
+        // But, It can also be simplified using the ternary operator.
+
+        grade >= 60 ? std::cout << "You passed the exam!" << '\n' : std::cout << "You failed the exam." << '\n';
+
+        --------------------------------------------------------------------------------------------------------
+
+        // EXAMPLE 2:
+
+        // Below is a code using ternary operator to check if a number is even or odd.
+
+        int number = 9;
+        number % 2 ? std::cout << "The number is even." << '\n' : std::cout << "The number is odd." << '\n';
+
+        --------------------------------------------------------------------------------------------------------
+
+        // EXAMPLE 3:
+
+        // Below is a code using ternary operator to check if a user is hungry or not.
+
+        bool hungry = true;
+        hungry ? std::cout << "You are hungry." : std::cout << "You are not hungry.";
+
+        // This line of code above can be simplified further by using the `std::cout` directly:
+        std::cout << (hungry ? "You are hungry." : "You are not hungry.") << '\n';
+
+        */
+
+        return 0;
+}
+
+/*
+Recap:
+- Ternary operator is a shorthand for if-else statements.
+- Syntax: condition ? expression_if_true : expression_if_false;
+- It can be used to simplify code and make it more readable.
+- It is often used for simple conditions where an if-else statement would be too verbose.
+- It can be used to assign values based on a condition.
+- It is a good practice to use it for simple conditions, but for more complex conditions it is better to use if-else statements for clarity.
+*/
