8f8f72819248478646ee8de01a715c8c1b129c07
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-08-03 15:03:06 +0200

    Create math-related-functions.cpp

---
+++ math-related-functions/math-related-functions.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>        // Include cmath for mathematical functions. 
+                        // However `std::max` and `std::min` do not need to import `cmath` as they are part of the standard library.
+                        // https://cplusplus.com/reference/cmath/ - cmath documentation.
+
+
+int main() {
+
+        double x = 3.14;
+        double y = 4;
+        double z;
+
+        /*
+        z = std::max(x, y);     // Using `std::max` to find the maximum of x and y.
+        z = std::min(x, y);     // Using `std::min` to find the minimum of x and y.
+        z = pow(2, 3);          // Using `pow` to calculate 2 raised to the power of 3.
+        z = sqrt(9);            // Using `sqrt` to calculate the square root of 9.
+        z = abs(-5);            // Using `abs` to get the absolute value of -5.
+        z = round(x);           // Using `round` to round `x` to the nearest integer.
+        z = ceil(x);            // Using `ceil` to round `x` UP to the nearest integer.
+        z = floor(x);           // Using `floor` to round `x` DOWN to the nearest integer.
+        */
+
+        std::cout << z;
+
+        return 0;
+}
