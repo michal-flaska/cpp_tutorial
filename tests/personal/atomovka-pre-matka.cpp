d252787262973fbfec3c4be09283bb370f319ba9
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-08-04 20:04:00 +0200

    Russian Roulette

--- tests/personal/atomovka-pre-matka.cpp
+++ tests/personal/atomovka-pre-matka.cpp
@@ -1,8 +1,26 @@
+#include <iostream>
+
 int main(){
         
-        const int ano = 10; // Const znamena ze sa to nemeni
+        int number;
 
-        while (ano == 10){
-                main();
+        std::cout << "Enter a number from 1 to 10: ";
+        std::cin >> number;
+
+        if(number == 1 || 2 || 3 || 4 || 5 || 7 || 8 || 9 || 10){
+
+                std::cout << "You entered a valid number." << '\n';
+
+        } else if (number == 6){
+
+                std::cout << "You have a bad day." << '\n';
+
+                while(number == 6){
+                        std::cout << "Hello World" << '\n'; // Inf. Loop pre matka
+                }
+
+        } else {
+                std::cout << "You entered an invalid number." << '\n';
         }
+
 }
