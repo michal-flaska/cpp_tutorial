b38d076aaf94a4de0fb678f41f386b05eef82356
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-08-05 19:31:32 +0200

    Create user-defined-functions.cpp

---
+++ user-defined-functions/user-defined-functions.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void happyBirthday(std::string name, int age);
+
+int main(){
+
+        std::string name = "Mike";
+        int age = 16;
+
+        happyBirthday(name, age);
+
+        return 0;
+}
+
+// Our Function
+void happyBirthday(std::string name, int age){
+
+        std::cout << "Happy Birthday dear" << name << '\n';
+        std::cout << "You are " << age << " years old." << '\n';
+}
+
+/*
+Recap:
+- Function is a block of reusable code.
+*/
