63b42f3c304cc3ba82e05b9b9a6ed2050bceec9d
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-08-05 14:32:06 +0200

    Create nested-loop.cpp

---
+++ loops/nested-loop/nested-loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+
+        for(int i = 1; i <= 3; i++){
+                for(int j = 1; j <= 10; j++){
+                        std::cout << j << ' ';
+                }
+                std::cout << '\n'; // New line after each inner loop iteration
+        }
+
+        return 0;
+}
