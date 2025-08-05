f0e2856207a9075fe767d6dfc537e065b751ea94
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-08-05 18:02:51 +0200

    Fix the bug in number guessing game

--- tests/from-tutorial/number-guessing-game/number-guessing-game.cpp
+++ tests/from-tutorial/number-guessing-game/number-guessing-game.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <ctime>
 
-// There's a bug that I don't know how to fix. Every time I finish a game, the total number of tries is shown as 32,764 or higher. I'm unable to find the problem.
-
 int main(){
 
         int num;
         int guess;
-        int tries;
+        int tries = 0;  // initialize to zero, this fixed the bug where the total number of tries was shown as 32,764
+                        // previously, it was `int tries;`
+                        // Sometimes the memory where tries lives just happens to be zero or a small value on that system, that compiler, that execution.
+                        // machine, compiler, OS, or runtime context probably left garbage in that memory slot.
 
         srand(time(NULL));
 
