eb1d854b82a8fd139b1072b19bc46a85a9bc4e02
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-08-05 17:43:27 +0200

    Create random-event-generator.cpp

---
+++ tests/from-tutorial/random-event-generator/random-event-generator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ctime>
+
+int main(){
+
+        srand(time(0));
+        int randNum = rand() % 5 + 1;
+
+        switch(randNum){
+                case 1: std::cout << "You Win a bumper sticker!" << '\n';
+                        break;
+                case 2: std::cout << "You Win a t-shirt!" << '\n';
+                        break;
+                case 3: std::cout << "You Win a lunch!" << '\n';
+                        break;
+                case 4: std::cout << "You Win a giftcard!" << '\n';
+                        break;
+                case 5: std::cout << "You Win a concert tickets!" << '\n';
+                        break;
+        }
+
+        return 0;
+}
