1ef559a55dde6a243a927919a2f01f1e9be9c1fa
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-08-11 14:08:30 +0200

    Finish banking-practice-program.cpp

--- tests/banking-practice-program/banking-practice-program.cpp
+++ tests/banking-practice-program/banking-practice-program.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip> // There is a function in here to set some precision for floating point numbers.
 
 void showBalance(double balance);
 double deposit();
@@ -10,19 +11,24 @@ int main(){
         int choice = 0;
 
         do{
-                std::cout << "Enter your choice: ";
+                std::cout << "Enter your choice: " << '\n';
                 std::cout << "1. Show Balance" << '\n';
                 std::cout << "2. Deposit Money" << '\n';
                 std::cout << "3. Withdraw Money" << '\n';
                 std::cout << "4. Exit" << '\n';
                 std::cin >> choice;
 
+                std::cin.clear();
+                fflush(stdin);
+
                 switch(choice){
                         case 1: showBalance(balance);
                                 break;
                         case 2: balance += deposit(); // Shortened form of `balance = balance + deposit();`
+                                showBalance(balance);
                                 break;
                         case 3: balance -= withdraw(balance);
+                                showBalance(balance);
                                 break;
                         case 4: std::cout << "Thanks for visiting!" << '\n';
                                 break;
@@ -32,17 +38,47 @@ int main(){
 
         return 0;
 }
-
+/*
+---------------------------------------------------------------------------------------------------------
+----------------------------------------------{ FUNCTIONS }----------------------------------------------
+---------------------------------------------------------------------------------------------------------
+*/
 void showBalance(double balance){
-
+        std::cout << "Your current balance is: $" << std::setprecision(2) << std::fixed << balance << '\n';
 }
 
 double deposit(){
 
-        return 0;
+        double amount = 0;
+
+        std::cout << "Enter the amount the be deposited: ";
+        std::cin >> amount;
+
+        if(amount > 0){
+                return amount;
+        }
+        else{
+                std::cout << "That's not a valid amount" << '\n';
+                return 0;
+        }
 }
 
 double withdraw(double balance){
 
-        return 0;
+        double amount;
+
+        std::cout << "Enter the amount you would like to withdraw: ";
+        std::cin >> amount;
+
+        if(amount > balance){
+                std::cout << "Insufficient funds" << '\n';
+                return 0;
+        }
+        else if(amount < 0){
+                std::cout << "That's not a valid amount" << '\n';
+                return 0;
+        }
+        else{
+                return amount;
+        }
 }
