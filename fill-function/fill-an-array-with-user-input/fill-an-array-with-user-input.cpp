a47c1834add29c0349d61837889803f9f3a6012b
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-10-19 15:16:08 +0200

    Update fill-an-array-with-user-input.cpp

--- fill-function/fill-an-array-with-user-input/fill-an-array-with-user-input.cpp
+++ fill-function/fill-an-array-with-user-input/fill-an-array-with-user-input.cpp
@@ -5,16 +5,24 @@ int main(){
 
         std::string foods[5];
         int size = sizeof(foods)/sizeof(foods[0]);
+        std::string temp;
 
         for(int i = 0; i < size; i++){
-                std::cout << "enter a food you like #" << i + 1 << ": ";
-                std::getline(std::cin, foods[i]);
+                std::cout << "enter a food you like or press 'q' to quit #" << i + 1 << ": ";
+                std::getline(std::cin, temp);
+
+                if(temp == "q"){
+                        break;
+                }
+                else{
+                        foods[i] = temp;
+                }
         }
 
-        std::cout << "You like the following food:";
+        std::cout << "You like the following food:" << '\n';
 
-        for(std::string food : foods){
-                std::cout << food << '\n';
+        for(int i = 0; !foods[i].empty(); i++){
+                std::cout << foods[i] << '\n';
         }
 
         return 0;
