433acb3d961d368e2c69dd74c3bf07c34f22aaa3
Michal Flaška <109202734+pilot2254@users.noreply.github.com>
2026-01-24 14:13:16 +0100

    Create function-templates.cpp

---
+++ function-templates/function-templates.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// template <typename T>
+template <typename T, typename U>
+
+auto max(T x, U y){
+        return(x > y) ? x : y;
+}
+
+int main(){
+
+        std::cout << max(1, 2) << '\n';
+        std::cout << max(1.2, 2.3) << '\n';
+        std::cout << max('1', '2') << '\n';
+
+        return 0;
+}
